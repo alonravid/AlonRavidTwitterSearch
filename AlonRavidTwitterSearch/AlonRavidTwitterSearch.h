c56ca2ed9b241b73dab488dd740ecab2f73286af
Alon Ribovsky <alonri75@walla.com>
2017-04-17 09:30:10 +0300

    Initial Commit

---
+++ AlonRavidTwitterSearch/AlonRavidTwitterSearch.h
@@ -0,0 +1,19 @@
+//
+//  AlonRavidTwitterSearch.h
+//  AlonRavidTwitterSearch
+//
+//  Created by Alon Ribovsky on 4/17/17.
+//  Copyright © 2017 Alon Ribovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlonRavidTwitterSearch.
+FOUNDATION_EXPORT double AlonRavidTwitterSearchVersionNumber;
+
+//! Project version string for AlonRavidTwitterSearch.
+FOUNDATION_EXPORT const unsigned char AlonRavidTwitterSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlonRavidTwitterSearch/PublicHeader.h>
+
+
